e815de7130ce61d0262e29d29940ffec7e45899b
Thomas Hepner <hepner.thomas@gmail.com>
2022-07-20 11:22:42 -0700

    feat: Add functional instantiation pattern of stack.js with all tests passing.

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,12 +6,27 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    var instanceLength = someInstance.size();
+    if (instanceLength === 0) {
+      storage[0] = value;
+    } else {
+      storage[instanceLength] = value;
+    }
   };
 
   someInstance.pop = function() {
+    var instanceLength = someInstance.size();
+    if (instanceLength === 0) {
+      return undefined;
+    } else {
+      var value = storage[instanceLength - 1];
+      delete storage[instanceLength - 1];
+      return value;
+    }
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
