ade0869dd5c67edfab07c316281b7b476da4fb14
Thomas Hepner <hepner.thomas@gmail.com>
2022-07-20 15:39:39 -0700

    Add ES6 implementation of stack.js with all tests passing.

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  // Add size method
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+  // Add push method
+  push(value) {
+    this.storage[this.size()] = value;
+  }
+
+  // Add pop method
+  pop() {
+    let value = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return value;
   }
 
 }
