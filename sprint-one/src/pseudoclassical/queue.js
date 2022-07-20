a2e39a8c9384425dff4344c70217b99c4fa69af8
Thomas Hepner <hepner.thomas@gmail.com>
2022-07-20 15:13:53 -0700

    feat: Add pseudoclassical implementation of queue.js with all tests passing.

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,49 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
+
+// add enqueue method
+Queue.prototype.enqueue = function(value) {
+  // get queue length
+  var queueSize = this.size();
+
+  // create a new array with value
+  var values = [value];
+
+  // get keys from queue and iterate through them
+  var queueKeys = Object.keys(this.storage);
+  for (let key in queueKeys) {
+    // get value at each key and add to values array
+    var value = this.storage[key];
+    values.push(value);
+  }
+
+  // iterate through values
+  for (let i = 0; i < values.length; i++) {
+    // set value at i in queue to corresponding element in values array
+    this.storage[i] = values[i];
+  }
+
+}
+
+// add dequeue method
+Queue.prototype.dequeue = function() {
+  // get queue length
+  var queueSize = this.size();
+
+  // get element at top of queue and delete it from the queue
+  var value = this.storage[queueSize - 1];
+  delete this.storage[queueSize - 1];
+
+  // return element
+  return value;
+}
+
+// add size method
+Queue.prototype.size = function() {
+  return Object.keys(this.storage).length;
+}
 
 
