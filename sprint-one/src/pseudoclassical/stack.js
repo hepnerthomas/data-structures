dae4b30e1bce3cced4e97c0f23e76f9c51988597
Thomas Hepner <hepner.thomas@gmail.com>
2022-07-20 15:13:29 -0700

    feat: Add pseudoclassical implementation of stack.js with all tests passing.

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,23 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
 
+// add push method
+Stack.prototype.push = function(value) {
+  this.storage[this.size()] = value;
+}
+
+// add pop method
+Stack.prototype.pop = function() {
+  var value = this.storage[this.size() - 1];
+  delete this.storage[this.size() - 1];
+  return value;
+}
+
+// add size method
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+}
