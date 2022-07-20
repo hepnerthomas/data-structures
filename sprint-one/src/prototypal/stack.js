ff7da7af010d1bf00729c118f8953ba91cff62f0
Thomas Hepner <hepner.thomas@gmail.com>
2022-07-20 14:44:58 -0700

    feat: Add prototypal implementation of stack.js with all tests passing.

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,39 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  return newStack;
 };
 
 var stackMethods = {};
 
+// Add push to stackMethods
+stackMethods.push = function(value) {
+  // get stack length
+  var stackLength = this.size();
 
+  // add value to top of stack
+  this.storage[stackLength] = value;
+}
+
+// Add pop method to stackMethods
+stackMethods.pop = function() {
+  // get stack length
+  var stackLength = this.size();
+
+  // get value at top of stack
+  var value = this.storage[stackLength - 1];
+
+  // delete value from stop of stack
+  delete this.storage[stackLength - 1];
+
+  // return value at top of stack
+  return value;
+
+}
+
+// Add size to stackMethods
+stackMethods.size = function() {
+  return Object.keys(this.storage).length;
+}
